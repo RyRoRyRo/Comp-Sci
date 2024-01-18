9ad305752af0431c9d605356b8c570c9aea1e521
RyRoRyRo <97533818+RyRoRyRo@users.noreply.github.com>
2024-01-18 11:37:15 -0500

    Create TicTacToe.java

---
+++ Comp SCI/Summative/TicTacToe.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    // global variables
+    private static String[] board = new String[9];
+    private static int turn = 0;
+    private static boolean playing = false;
+
+    private static Player player1 = new Player(1, "X", false);
+    private static Player player2 = new Player(2, "O", false);
+
+    public static void main(String[] args) {
+        
+    }
+    }
