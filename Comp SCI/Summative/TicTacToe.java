0ac119912c6a417a614f51c1bdfc59ff83e6b1ca
RyRo <97533818+RyRoRyRo@users.noreply.github.com>
2024-01-21 17:45:14 -0500

    Update TicTacToe.java

--- Comp SCI/Summative/TicTacToe.java
+++ Comp SCI/Summative/TicTacToe.java
@@ -1,3 +1,8 @@
+//January 12, 2023
+//Summative 2: TicTacToe
+//Ryan KS
+//Game where you try to get 3 in a row as either X's or O's. Additional features included.
+
 import java.util.Scanner;
 
 public class Main {
@@ -12,46 +17,49 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String symbol = "X";
         boolean multiplayer= true;
-        for (int i = 0; i < 9; i++){
+        for (int i = 0; i < 9; i++){ //fill blank board
             board[i] = "-";
         }
-        System.out.println("Please select an option: ");
+        System.out.println("Please select an option: "); //game options
         System.out.println("1. Local-multiplayer: ");
         System.out.println("2. Versus CPU:");
         int input = scanner.nextInt();
+        scanner.nextLine();
         if (input == 1) {
             multiplayer = true;
         }
         if (input == 2) {
             multiplayer = false;
         }
+        clearScreen(); //clear and print board
+        printBoard();
+        wait(300);
         while (playing) {
-            if (turn == 0){
+            if (turn == 0){ //switches turn symbol
                 symbol = "X";
             }
             if (turn == 1){
                 symbol = "O";
             }
-            clearScreen();
-            printBoard();
-            wait(300);
             if (multiplayer) {
-                System.out.println("Player " + (turn + 1) + "'s turn.");
+                System.out.println("Player " + (turn + 1) + "'s turn."); //tell players turn
                 System.out.println("Enter a board position to place your " + symbol + ": ");
-                int position = scanner.nextInt();
-                if (!board[position - 1].equals("-")) {
-                    System.out.println("Enter a valid board position.");
+                int position = scanner.nextInt(); //take input
+                scanner.nextLine();
+                if (!board[position - 1].equals("-")) { //turns input into array position
+                    System.out.println("Enter a valid board position."); //if its not a "-" then there must be something there making it an invalid move
                     wait(1000);
                 } else {
                     board[position - 1] = symbol;
-                    turn = (turn + 1) % 2;
+                    turn = (turn + 1) % 2; //switches turn
                 }
             }
             else {
                 if (turn == 0) {
-                    System.out.println("Enter a board position to place your " + symbol + ": ");
+                    System.out.println("Enter a board position to place your " + symbol + ": "); //players turn
                     int position = scanner.nextInt();
-                    if (!board[position - 1].equals("-")) {
+                    scanner.nextLine();
+                    if (!board[position - 1].equals("-")) { //invalid turn checker
                         System.out.println("Enter a valid board position.");
                         wait(1000);
                     } else {
@@ -59,51 +67,59 @@ public class Main {
                         turn = (turn + 1) % 2;
                     }
                 }
-                else {
+                else { //cpus turn
                     System.out.println("Waiting for CPU...");
-                    board[cpuMove()] = "O";
+                    board[cpuMove()] = "O"; //makes random move
                 }
             }
-            playing = checkWin(symbol);
-            win = draw();
+            clearScreen(); //clear and reprint board
+            printBoard();
+            wait(300);
+            playing = checkWin(symbol); //check if player just won
+            win = draw(); //check for draw
         }
-        System.out.println(symbol + "'s Win!");
+        wait(200);
+        System.out.println("");
         if (win) {
+            System.out.println(symbol + "'s Win!"); //announce winner
             if (turn == 1) {
-                scores[0]++;
+                scores[0]++; //add score
             }
             if (turn == 0) { //switched because turns switch before win is checked
                 if (multiplayer) {
-                    scores[1]++;
+                    scores[1]++; //add score
                 } else {
-                    scores[2]++;
+                    scores[2]++; //add CPU score
                 }
             }
             wait(500);
         }
         else {
-            System.out.println("Draw!");
+            System.out.println("Draw!"); //announce draw
             wait(500);
 
         }
-        System.out.println("Player 1 Wins: " + scores[0] + " Player 2 Wins: " + scores[1] + " CPU Wins: " + scores[2]);
-        System.out.println("Play again? (y/n)");
-        String playagain = scanner.nextLine();
-        switch (playagain.toLowerCase()) {
+        System.out.println("Player 1 Wins: " + scores[0] + " | Player 2 Wins: " + scores[1] + " | CPU Wins: " + scores[2]); //play again logic
+        System.out.println("Play again? (y / n)");
+        String playAgain = scanner.nextLine();
+        switch (playAgain.toLowerCase()) {
             case "y" -> {
+                playing = true;
+                turn = 0;
+                win = true;
                 main(args); //restart program
+                break;
             }
             case "n" -> {
                 break; //end program
             }
-            default -> throw new IllegalStateException("Unexpected value: " + playagain.toLowerCase());
         }
 
     }
 
-    private static boolean draw(){
+    private static boolean draw(){ //check for draw
         for (int i = 0; i < 9; i++){
-            if (board[i].equals("-")) {
+            if (board[i].equals("-")) { //if everything ISNT "-" then its a draw
                 return true;
             }
         }
@@ -111,7 +127,7 @@ public class Main {
     }
 
 
-    private static void printBoard(){
+    private static void printBoard(){ //print board
         System.out.println(board[0] + " " + board[1] + " " + board[2]);
         System.out.println(board[3] + " " + board[4] + " " + board[5]);
         System.out.println(board[6] + " " + board[7] + " " + board[8]);
@@ -119,15 +135,15 @@ public class Main {
     }
     private static int cpuMove(){
         wait(700);
-        int position = (int)(Math.random() * 9);
-        while (!board[position].equals("-")) {
+        int position = (int)(Math.random() * 9); //pick random move from 0-8
+        while (!board[position].equals("-")) { //redoes it if invalid
             position = (int)(Math.random() * 9);
         }
         wait(1000);
-        turn = (turn + 1) % 2;
+        turn = (turn + 1) % 2; //switches turn
         return position;
     }
-    public static boolean checkWin(String symbol) {
+    public static boolean checkWin(String symbol) { //check for win logic
         if (board[0].equals(symbol) && board[1].equals(symbol) && board[2].equals(symbol)) {
             return false;
         }
