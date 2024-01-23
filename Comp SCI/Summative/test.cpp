ce43723236d402f5e4ef5ecada271b7ed4b61927
RyRo <97533818+RyRoRyRo@users.noreply.github.com>
2024-01-22 23:37:01 -0500

    Update test.cpp

--- Comp SCI/Summative/test.cpp
+++ Comp SCI/Summative/test.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void printBoard(int size, string arr[10][10]){
-    cout << "Printing Board:\n";
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            cout << "\t" << arr[i][j];
-        }
-        cout << endl;
-    }
-}
 
-void playerMove(int size, string arr[10][10]){
+void printBoard(int length, string arr[][10]);
+
+void playerMove(int size, string arr[10][10]) {
     int row, col;
     cout << "Enter the coordinates (row and column) to set to 'X': ";
     cin >> row >> col;
@@ -21,41 +14,57 @@ void playerMove(int size, string arr[10][10]){
     } else {
         cout << "Invalid coordinates. Exiting program." << endl;
     }
-
-    
 }
 
 int main() {
     int length = 0;
     bool invalidLength = true;
+    int winCondition = 3;
+
+
     while (invalidLength) {
-    cout << "Enter Board Length:" << endl;
-    cin >> length;
-    if (length < 3) {
-        cout << "Enter a valid Board Length (3-10)" << endl;
+        cout << "Enter Board Length:" << endl;
+        cin >> length;
+
+        if (length < 3 || length > 10) {
+            cout << "Invalid Board Length. Enter a value between 3 and 10." << endl;
+        } else {
+            invalidLength = false;
+        }
     }
-    else if (length > 10) {
-        cout << "Enter a valid Board Length (3-10)" << endl;
-    }
-    else {
-        invalidLength = false;
-    }
+    invalidLength = true;
+     while (invalidLength) {
+        cout << "Enter Ammount in a row needed to win:" << endl;
+        cin >> winCondition;
+
+        if (winCondition < 3 || winCondition => length) {
+            cout << "Invalid win condition Length. Enter a value between 3 and " << length << "." << endl;
+        } else {
+            invalidLength = false;
+        }
     }
     
-    string arr[length][length];
-    
+    string arr[10][10];
+
     for (int i = 0; i < length; i++) {
         for (int j = 0; j < length; j++) {
             arr[i][j] = "-";
         }
     }
-    printBoard(length, arr);
-    
-    playerMove(length);
-    
 
+    printBoard(length, arr);
+    playerMove(length, arr);
+    printBoard(length, arr);
 
     return 0;
 }
 
-
+void printBoard(int length, string arr[][10]) {
+    cout << "Printing Board:\n";
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < length; j++) {
+            cout << "\t" << arr[i][j];
+        }
+        cout << endl;
+    }
+}
