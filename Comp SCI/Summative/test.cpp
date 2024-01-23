0dc3dde9fd32f390322e20fc5feabb337a2bffa3
RyRo <97533818+RyRoRyRo@users.noreply.github.com>
2024-01-22 23:46:43 -0500

    Update test.cpp

--- Comp SCI/Summative/test.cpp
+++ Comp SCI/Summative/test.cpp
@@ -4,24 +4,35 @@ using namespace std;
 
 void printBoard(int length, string arr[][10]);
 
-void playerMove(int size, string arr[10][10]) {
+void playerMove(int size, string arr[10][10], char symbol) {
     int row, col;
-    cout << "Enter the coordinates (row and column) to set to 'X': ";
-    cin >> row >> col;
+    cout << "Enter the coordinates (X Y) to set to '" << symbol << "': ";
+    cin >> col >> row;
 
-    if (row >= 0 && row < size && col >= 0 && col < size) {
-        arr[row][col] = "X";
+    if (row - 1 >= 0 && row -1 < size && col - 1 >= 0 && col - 1 < size) {
+        arr[row - 1][col - 1] = symbol;
     } else {
         cout << "Invalid coordinates. Exiting program." << endl;
     }
 }
 
+void changeTurn(char symbol){
+    if (symbol == 'X'){
+        symbol = 'O';
+    }
+    if (symbol == 'O'){
+        symbol = 'X';
+    }
+}
+    
+
 int main() {
     int length = 0;
     bool invalidLength = true;
     int winCondition = 3;
-
-
+    char symbol = 'X';
+    bool playing = true;
+    
     while (invalidLength) {
         cout << "Enter Board Length:" << endl;
         cin >> length;
@@ -37,7 +48,7 @@ int main() {
         cout << "Enter Ammount in a row needed to win:" << endl;
         cin >> winCondition;
 
-        if (winCondition < 3 || winCondition => length) {
+        if (winCondition < 3 || winCondition > length) {
             cout << "Invalid win condition Length. Enter a value between 3 and " << length << "." << endl;
         } else {
             invalidLength = false;
@@ -51,11 +62,14 @@ int main() {
             arr[i][j] = "-";
         }
     }
-
+    
+    
+    while (playing) {
     printBoard(length, arr);
-    playerMove(length, arr);
-    printBoard(length, arr);
-
+    playerMove(length, arr, symbol);
+    changeTurn(symbol);
+    }
+    
     return 0;
 }
 
